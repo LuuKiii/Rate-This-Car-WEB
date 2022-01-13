2b8f49672ab9be9e50105dccdeb03980d478534d
Lucas <lukasz.kurdziel99@gmail.com>
2022-01-13 17:25:26 +0100

    Commit 1.4
    Preparation for login and registration

---
+++ src/main/java/login/LoginBB.java
@@ -0,0 +1,72 @@
+package login;
+
+
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.faces.context.Flash;
+import javax.faces.view.ViewScoped;
+import javax.servlet.http.HttpSession;
+
+
+import jsf.dao.UserDAO;
+import jsf.entities.User;
+
+@Named
+@RequestScoped
+public class LoginBB {
+	private static final String PAGE_STAY_AT_THE_SAME = null;
+	
+	private String login;
+	private String pass;
+	
+	
+	
+	public String getLogin() {
+		return login;
+	}
+
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+
+	public String getPass() {
+		return pass;
+	}
+
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+
+	@Inject
+	ExternalContext extcontext;
+	
+	@Inject
+	FacesContext ctx;
+	
+	@Inject
+	Flash flash;
+	
+	@EJB
+	UserDAO userDAO;
+	
+
+	public List<User> getFullList(){
+		return userDAO.getAllUsers();
+	}
+		
+
+}
