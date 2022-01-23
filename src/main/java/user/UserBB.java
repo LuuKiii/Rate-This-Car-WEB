c985d07fb66cbc63dc06c629cd0b4bf0a4a32c76
Lucas <lukasz.kurdziel99@gmail.com>
2022-01-23 23:05:39 +0100

    commit 9th
    Added userpage.

--- src/main/java/user/UserBB.java
+++ src/main/java/user/UserBB.java
@@ -14,17 +14,39 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.faces.context.Flash;
+import javax.faces.simplesecurity.RemoteClient;
 import javax.faces.view.ViewScoped;
 import javax.servlet.http.HttpSession;
 
 
 import jsf.dao.UserDAO;
+import jsf.dao.UserRatesVehicleDAO;
+import jsf.dao.VehicleDAO;
 import jsf.entities.User;
+import jsf.entities.UserRatesVehicle;
+import jsf.entities.Vehicle;
 
 @Named
 @RequestScoped
 public class UserBB {
+	private User user = new User();
+	private UserRatesVehicle rates = new UserRatesVehicle();
+	private RemoteClient<User> userTemp = new RemoteClient<User>();
 	
+	private Vehicle vehicle = new Vehicle();
+	
+	
+	
+	public Vehicle getVehicle() {
+		return vehicle;
+	}
+
+
+	public void setVehicle(Vehicle vehicle) {
+		this.vehicle = vehicle;
+	}
+
+
 	@Inject
 	ExternalContext extcontext;
 	
@@ -37,10 +59,37 @@ public class UserBB {
 	@EJB
 	UserDAO userDAO;
 	
-
-	public List<User> getFullList(){
-		return userDAO.getAllUsers();
+	@EJB
+	VehicleDAO vehicleDAO;
+	
+	@EJB
+	UserRatesVehicleDAO rateDAO;
+	
+	public User getCurrentUser() {
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		HttpSession session = (HttpSession) facesContext.getExternalContext().getSession(false);
+		userTemp = (RemoteClient<User>)session.getAttribute("remoteClient");
+		user = userDAO.find(userTemp.getDetails().getIduser());
+		
+		return user;
+	}
+	
+	
+	public List<UserRatesVehicle> getUsersList(){
+		return rateDAO.getUsersList(getCurrentUser());
 	}
 		
+	
+
+	
+	public Vehicle getVeh(int idv) {
+		Vehicle veh = vehicleDAO.find(idv);
+		return veh;
+	}
+	
+	public int intPars(double val) {
+		int transval = (int) val;
+		return transval;
+	}
 
 }
